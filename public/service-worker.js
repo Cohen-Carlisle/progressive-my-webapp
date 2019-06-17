8c516cb8014046729d8f756bc9ff243a48992075
Cohen Carlisle <cohencarlisle+github@pm.me>
2019-06-17 16:36:30 -0400

    finalize service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,17 @@
 var CACHE_NAME = "cache-v1";
-self.addEventListener("install", (e) => {
-  e.waitUntil(
+var urlsToCache = ["/offline.html"]
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache => {
-      return cache.addAll(["/offline.html"]);
+      return cache.addAll(urlsToCache);
     }))
   );
 });
 
 // hit the network and fallback to cached info
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
       // if network call fails, load the cached info
       return caches.open(CACHE_NAME).then((cache) => {
         return cache.match("offline.html");
